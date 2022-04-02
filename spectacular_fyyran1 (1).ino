3c2ddc76e79c328efa9f58cad171e96b8ed4b2f3
mohsin-cyber <52150458+mohsin-cyber@users.noreply.github.com>
2022-04-02 14:01:04 +0500

    arduino light project

---
+++ spectacular_fyyran1 (1).ino
@@ -0,0 +1,74 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(1, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(2, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(3, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(4, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(5, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(5, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(6, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(6, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(13, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(12, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(11, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(11, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(10, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(10, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(8, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(8, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+}
